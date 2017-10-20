766234dedbb0af14a840f3c87d08eb32de95e417
Nivelin Yovchev <nivelin.yovchev@outlook.com>
2017-10-20 19:12:22 +0300

    Dictionary

--- Programming Fundamentals - Exercises/07.Lists-Lab/01.RemoveNegativesAndReverse/RemoveNegativesAndReverse.cs
+++ Programming Fundamentals - Exercises/07.Lists-Lab/01.RemoveNegativesAndReverse/RemoveNegativesAndReverse.cs
@@ -11,10 +11,22 @@ namespace _01.RemoveNegativesAndReverse
         static void Main(string[] args)
         {
             List <int> numbers =Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> result = new List<int>();
 
-            for (int i = 0; i <numbers.Count; i++)
+            for (int i = numbers.Count-1; i >=0; i--)
             {
-                if(numbers<i>)
+                if(numbers[i] >=0)
+                {
+                    result.Add(numbers[i]);
+                }
+            }
+            if(result.Count>0)
+            {
+                Console.WriteLine(string.Join(" ", result));
+            }
+            else
+            {
+                Console.WriteLine("empty");
             }
         }
     }
