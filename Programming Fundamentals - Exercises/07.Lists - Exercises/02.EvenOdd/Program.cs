1272af888ed8d1f4cd245794234d5de1ad648920
Nivelin Yovchev <nivelin.yovchev@outlook.com>
2017-10-21 18:59:02 +0300

    Dictionary

---
+++ Programming Fundamentals - Exercises/07.Lists - Exercises/02.EvenOdd/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+               input = "";
+
+            while((commands != "Even")||(commands != "Odd"))
+            {
+                input = Console.ReadLine();
+                string[] commands = input.Split();
+
+                if (commands=="Delete")
+                {
+                    foreach()
+                }
+                else if (commands == "Insert")
+                {
+                    foreach ()
+                }
+                commands = Console.ReadLine();
+
+            }
+            if (commands=="Even")
+            {
+
+            }
+            else if (commands == "Odd")
+            {
+
+            }
+        }
+    }
+}
