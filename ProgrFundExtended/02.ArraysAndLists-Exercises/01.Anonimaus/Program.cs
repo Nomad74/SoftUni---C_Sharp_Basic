452cdc14b8db22f4e610fcd55d7d79976f92a92f
Nivelin Yovchev <nivelin.yovchev@outlook.com>
2017-12-03 22:32:19 +0200

    Dictionary

---
+++ ProgrFundExtended/02.ArraysAndLists-Exercises/01.Anonimaus/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Anonimaus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commands = new string[3];
+
+            var inputString = Console.ReadLine().Split(' ').ToArray();
+
+
+            while (commands[0]!="3:1")
+            {
+                commands = Console.ReadLine().Split(' ').ToArray();
+
+                var startIndex = int.Parse(commands[1]);
+                var endIndex = int.Parse(commands[2]);
+
+                switch (commands[0])
+                {
+                    case "merge":
+
+                        for (int i = startIndex; i < endIndex; i++)
+                        {
+
+                        }
+
+                    case "divide":
+                }
+            }
+        }
+    }
+}
---
+++ ProgrFundExtended/04.DictionariesAndLINQ/01.OddOccurrences/OddOccurrences.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.OddOccurrences
+{
+    class OddOccurrences
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToLower().Split(' ').ToArray();
+            Dictionary<string, int> inputToDict = new Dictionary<string, int>();
+
+            foreach (var word in input)
+            {
+                if (inputToDict.ContainsKey(word))
+                {
+                    inputToDict[word]++;
+                }
+                else
+                {
+                    inputToDict[word]=1;
+                }
+            }
+            var result = new List<string>();
+
+            foreach (var word in inputToDict)
+            {
+                if (word.Value %2 !=0)
+                {
+                     result.Add(word.Key);
+                }
+            }
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
